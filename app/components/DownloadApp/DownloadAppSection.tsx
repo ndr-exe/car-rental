58bd0099533301df9650c2096bcff721dcfa4723
Nodar Miminoshvili <miminoshvilinodar@gmail.com>
2024-07-10 18:48:51 +0400

    Style Download-Mobile-App section for all screen sizes

--- app/components/DownloadApp/DownloadAppSection.tsx
+++ app/components/DownloadApp/DownloadAppSection.tsx
@@ -6,21 +6,39 @@ import { poppins } from '@/app/fonts';
 const DownloadAppSection = () => {
   return (
     <div className="bg-[#f8f8f8]">
-      <section className="container">
-        <div className="py-24 px-6 flex flex-col gap-5 text-center">
-          <h2 className={`${poppins.className} text-[2.65rem] font-bold`}>
-            Download our app to get most out of it
-          </h2>
-          <p className="text-secondary leading-normal">
-            Thrown shy denote ten ladies though ask saw. Or by to he going think order event music.
-            Incommode so intention defective at convinced. Led income months itself and houses you.
-          </p>
-          <div className="flex flex-col items-center gap-7 pt-5">
-            <Image src={playStoreIcon} alt="Google Play Store Icon" width={220} height={67} />
-            <Image src={appStoreIcon} alt="App Store Icon" width={220} height={67} />
+      <div className="fullHDWrapper DownloadMobileAppSectionBg ">
+        <section className="container ">
+          <div
+            className="py-24 px-6 flex flex-col gap-5 text-center max-w-[34rem] mx-auto sm:px-0 
+          md:mx-0 md:pl-6 md:max-w-[calc(34rem+24px)] md:text-left md:items-start "
+          >
+            <h2 className={`${poppins.className} text-[2.65rem] font-bold`}>
+              Download our app to get most out of it
+            </h2>
+            <p className="text-secondary leading-normal">
+              Thrown shy denote ten ladies though ask saw. Or by to he going think order event
+              music. Incommode so intention defective at convinced. Led income months itself and
+              houses you.
+            </p>
+            <div className="flex flex-col items-center gap-7 pt-5 sm:flex-row sm:justify-center">
+              <Image
+                src={playStoreIcon}
+                alt="Google Play Store Icon"
+                width={220}
+                height={67}
+                className="w-[13.5rem] h-auto"
+              />
+              <Image
+                src={appStoreIcon}
+                alt="App Store Icon"
+                width={220}
+                height={67}
+                className="w-[13.5rem] h-auto"
+              />
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 };
